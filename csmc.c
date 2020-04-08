8a9567a4f2148550cd17f33813168f1f15cfc654
conlie <conlie@localhost.localdomain>
2020-04-08 17:47:27 -0500

    queue implemented

--- csmc.c
+++ csmc.c
@@ -16,95 +16,91 @@
 // If not enough chairs for student, they return to lab
 
 // GLOBAL VARS
-int NUM_STUDENTS;
-int NUM_TUTORS;
-int NUM_SEATS;
-int MAX_PRIORITY;
+int NUM_STUDENTS, NUM_TUTORS, NUM_SEATS, MAX_PRIORITY;
 
 int ts_val, ss_val;
 int students_tutored = 0;
 
-sem_t COORDINATOR; // for student to signal coordinator for queueing
-sem_t WAITING_ROOM; // for keeping track of students awaiting tutoring
-sem_t QUEUE_SEM; // for making tutors aware that a student(s) has been queued
-sem_t TUTOR_ROOM; // manage tutors available
-sem_t TUTORING; // for tutor to hold a student for tutoring
+sem_t 
+COORDINATOR,  // for student to signal coordinator for queueing
+WAITING_ROOM, // for keeping track of students awaiting tutoring
+QUEUE_SEM,    // for making tutors aware that a student(s) has been queued
+TUTOR_ROOM,   // manage tutors available
+TUTORING;     // for tutor to hold a student for tutoring
 
-pthread_mutex_t mut_lock;
+pthread_mutex_t SAVE;
 
-int * tutorList;
+struct tutor
+{
+	int id;
+	int students_tutored;
+};
+struct tutor *tutors;
 
 struct student 
 {
 	int id;
 	int priority;
+
 };
-struct student *studentList;
-int stuListCurSize;
+struct student *students;
+struct student *seats;
 
-
-/* PRIORITY QUEUE 
- * MAX-HEAP of waiting students
- */
-int CUR_SIZE = 0;
-struct student *QUEUE;
-
-static void swap(struct student *a, struct student *b)
+// QUEUE
+struct node
 {
-	struct student temp = *a;
-	*a = *b;
-	*b = temp;
+	struct student *stud;
+	struct node *next_in_line;
+}; 
+
+struct node *front;
+struct node *rear;
+
+// readability
+int is_empty()
+{
+	return (front == NULL);
 }
 
-static void heapify(struct student chairs[], int n, int i)
-{
-	int greatest = i;
-	int left = 2 * i + 1;
-	int right = 2 * i + 2;
-	if (left < n && chairs[left].priority > chairs[greatest].priority)
-		greatest = left;
-	if (right < n && chairs[right].priority > chairs[greatest].priority)
-		greatest = right;
-	if (greatest != i) 
-	{
-		swap(&chairs[i], &chairs[greatest]);
-		heapify(chairs, n, greatest);
+// returns <= 0 if failed
+int enqueue(struct student *value){
+
+	struct node *item;
+	item->stud = value;
+
+	if (item->stud->id == NULL)
+		return 0;
+
+	if(rear == NULL)
+		front = rear = item;
+	else{
+		rear->next_in_line = item;
+		rear = item;
 	}
+
+	return 1;
 }
 
-static void heapSort(struct student chairs[], int n)
-{
-	int i;
-	for (i = n / 2 - 1; i >= 0; i--)
-		heapify(chairs, n, i);
-	for (i = n - 1; i >= 0; i--) 
-	{
-		swap(&chairs[0], &chairs[i]);
-		heapify(chairs, i, 0);
+// tutor gets student off of queue
+struct student *dequeue(){
+
+	if(is_empty()){
+
+		printf("\nThe queue is empty!\n");
+		return NULL;
 	}
-}
 
-static void pushHeap(struct student chairs[], struct student s)
-{
-	chairs[CUR_SIZE] = s;
-	CUR_SIZE++;
-	heapSort(chairs, CUR_SIZE);
-}
+	struct node *temp = front;
+	front = front->next_in_line;
 
-static struct student popHeap(struct student chairs[])
-{
-	struct student ret = chairs[0];
-	CUR_SIZE--;
-	chairs[0] = chairs[CUR_SIZE];
-	heapSort(chairs, CUR_SIZE);
-	return ret;
-
+	return temp->stud;
 }
-// END MAX_HEAP
+// END QUEUE
 
 int done = 0;
 static void *coordinatorRoutine()
 {
+	struct student *student_holder;
 	int total_requests = 0;
 	while (1)
 	{
@@ -112,76 +108,123 @@ static void *coordinatorRoutine()
 		total_requests++;
 		sleep(0.2);
 		
-		
 		if (!done) {
-			pushHeap(QUEUE, studentList[0]);
+			for (int i = 1; i < NUM_SEATS; i++)
+				if (seats[i].priority > student_holder->priority)
+					*student_holder = seats[i];
+			
 			printf("Co: Student %d with priority %d in the queue. Waiting students now = %d. Total requests = %d.\n", 
-					studentList[0].id, 
-					studentList[0].priority, 
+					student_holder->id, 
+					student_holder->priority, 
 					NUM_SEATS - ss_val, 
 					total_requests);
 			
-
 			sem_post(&QUEUE_SEM);
 		}
 		else
 		{
 			break;
 		}
-
 	}
 }
 
 static void *tutorRoutine(void *id)
 {
-	long i = (long) id;
-
 	sem_post(&TUTOR_ROOM);
 
 	sem_wait(&QUEUE_SEM);
-	struct student curStu = popHeap(QUEUE);
 
 	sem_wait(&TUTOR_ROOM);
 
 	sem_post(&TUTORING);
-	printf("Tu: Student %d tutored by Tutor %d. Students tutored now = %d\n", curStu.id, tutorList[i]);
+	//printf("Tu: Student %d tutored by Tutor %d. Students tutored now = %d\n", tutorList[i].id);
 
-	tutorRoutine((void *) i);
+	if (!done)
+		tutorRoutine(id);
 }
 
 static void *studentRoutine(void *id) 
 {
-	long i = (long) id;
-
+	pthread_mutex_lock(&SAVE);
 	sem_getvalue(&WAITING_ROOM, &ss_val);
 	if (!ss_val) 
 	{
-		printf("St: Student %d found no empty chair. Will try again later\n", studentList[i].id);
-		sleep(random() % 2);
+		printf("St: Student %d found no empty chair. Will try again later\n", students[(long) id].id);
+		usleep(random() % 2);
 	}
 	sem_wait(&WAITING_ROOM);
-
 	sem_getvalue(&WAITING_ROOM, &ss_val);
-	printf("St: Student %d takes a seat. Empty chairs = %d\n", studentList[i].id, ss_val);
-	sleep(0.2);
+	printf("St: Student %d takes a seat. Empty chairs = %d\n", students[(long) id].id, ss_val);
+
+	seats[NUM_SEATS - ss_val] = students[(long) id];
+	pthread_mutex_unlock(&SAVE);
 
 	sem_post(&COORDINATOR);
-	printf("St: Student %d flagged coordinator\n", studentList[i].id);
-	sleep(0.2);
+	printf("St: Student %d flagged coordinator\n", students[(long) id].id);
 
 	sem_wait(&TUTOR_ROOM);
-	printf("St: Student %d recieved help from Tutor y\n", studentList[i].id);
-	studentList[i].priority--;
+	printf("St: Student %d recieved help from Tutor y\n", students[(long) id].id);
+	students[(long) id].priority--;
 
 	sem_post(&WAITING_ROOM);
-	printf("St: Student %d leaving waiting room\n", studentList[i].id);
-	sleep(0.2);
+	printf("St: Student %d leaving waiting room\n", students[(long) id].id);
+	usleep(0.2);
 
-	if (studentList[i].priority)
-		studentRoutine((void *) i);
-	else
-		return;
-	
+	if (students[(long) id].priority)
+		studentRoutine(id);
+}
+
+void *makeTuts()
+{
+  	long counter = 0;
+    while(counter < NUM_TUTORS)
+    {
+		// create a new tutor
+		struct tutor tut = {counter, 0};
+		tutors[counter] = tut;
+
+        // Declare and create a thread 
+        pthread_t tut_thread;
+        if (pthread_create(&tut_thread, NULL, tutorRoutine, (void *) counter) != 0)
+            printf("Failed to create thread for tutor.");
+        counter++;
+            
+        /* Sleep for 100ms before creating another customer */
+        usleep(100000);
+    }
+}
+
+void *makeStuds()
+{
+    long counter = 0;
+    while(counter < NUM_STUDENTS)
+    {
+		// create a new student 
+		struct student stud = {counter, MAX_PRIORITY};
+		students[counter] = stud;
+
+        /* Declare and create a thread */
+        pthread_t stud_thread;
+        if (pthread_create(&stud_thread, NULL, studentRoutine, (void *) counter) != 0)
+            printf("Failed to create thread for student.");
+        counter++;
+    }
+}
+
+void initGlobals(char **argv)
+{
+	NUM_STUDENTS = atoi(argv[1]);
+	NUM_TUTORS = atoi(argv[2]);
+	NUM_SEATS = atoi(argv[3]);
+	MAX_PRIORITY = atoi(argv[4]);
+
+	sem_init(&COORDINATOR, 0, 0);
+	sem_init(&WAITING_ROOM, 0, NUM_SEATS);
+	sem_init(&QUEUE_SEM, 0, 0);
+	sem_init(&TUTOR_ROOM, 0, 0);
+	sem_init(&TUTORING, 0, 0);
+
+	pthread_mutex_init(&SAVE, NULL);
 }
 
 void main(int argc, char **argv)
@@ -191,60 +234,28 @@ void main(int argc, char **argv)
 		printf("EXEC FORMAT: ./csmc #students #tutors #chairs #help\n");
 		exit(1);
 	}
-	NUM_STUDENTS = atoi(argv[1]);
-	NUM_TUTORS = atoi(argv[2]);
-	NUM_SEATS = atoi(argv[3]);
-	MAX_PRIORITY = atoi(argv[4]);
+	initGlobals(argv);
+	struct tutor tu[NUM_TUTORS];
+	struct student stu[NUM_STUDENTS], se[NUM_SEATS];
+	tutors = tu;
+	students = stu;
+	seats = se;
 
-	// ORGANIZE DATA COLLECTIONS
-	int tutAlloc[NUM_TUTORS];
-	struct student studentAlloc[NUM_STUDENTS];
-	struct student queueAlloc[NUM_SEATS];
+	pthread_t cor, tut_maker, stud_maker; // define initial threads
 
-	tutorList = tutAlloc;
-	QUEUE = queueAlloc;
-	studentList = studentAlloc;
-
-	pthread_t cor;
-	pthread_t tuts[NUM_TUTORS];
-	pthread_t stu[NUM_STUDENTS];
-
-	sem_init(&COORDINATOR, 0, 0);
-	sem_init(&WAITING_ROOM, 0, NUM_SEATS);
-	sem_init(&QUEUE_SEM, 0, 0);
-	sem_init(&TUTOR_ROOM, 0, 0);
-	sem_init(&TUTORING, 0, 0);
-
-	pthread_mutex_init(&mut_lock, NULL);
-	
-	pthread_create(&cor, NULL, coordinatorRoutine, NULL);
-	long i;
+	if (pthread_create(&cor, NULL, coordinatorRoutine, NULL) != 0)
+		printf("Faied to create thread for coordinator\n");
 
 	// CREATE TUTOR THREADS
-	for (i = 0; i < NUM_TUTORS; i++)
-	{
-		tutorList[i] = i+1;
-		pthread_create(&tuts[i], NULL, tutorRoutine, (void *) i);
-	}
+	if (pthread_create(&tut_maker, NULL, makeTuts, NULL) != 0)
+		printf("Faied to create thread for making tutors\n");
 
 	// CREATE STUDENT THREADS
-	stuListCurSize = 0;
-	for (i = 0; i < NUM_STUDENTS; i++)
-	{
-		struct student newStud = {i+1, MAX_PRIORITY};
-		studentList[i] = newStud;
-		pthread_create(&stu[i], NULL, studentRoutine, (void *) i);
-	}
+	if (pthread_create(&stud_maker, NULL, makeStuds, NULL) != 0)
+		printf("Faied to create thread for making students\n");
 
-	for (i = 0; i < NUM_STUDENTS; i++)
-	{
-		pthread_join(stu[i], NULL);
-	}
-
-	for (i = 0; i < NUM_TUTORS; i++)
-	{
-		pthread_join(tuts[i], NULL);
-	}
+	pthread_join(stud_maker, NULL);
+	pthread_join(tut_maker, NULL);
 
 	done = 1;
 	pthread_join(cor, NULL);
